c93baf1f315f1c1e100b40b71c15e5a03ed35305
Akhil Soni <85015174+Akhil9368@users.noreply.github.com>
2022-07-22 20:55:04 +0530

    Create Day_13.js

---
+++ Day_13.js
@@ -0,0 +1,26 @@
+//  Window vc Document
+//--->window is the Main container or we can say the global object at any opereatins related to entire browser , window can be the part of window object....
+
+// whereas dom is the child of window object
+// By the Help of location we find the current locations
+// html text--->document .. document.style.color ="red"
+// window related----> window object
+
+// window--->dom,bom,javascript....
+// Bom -->navigator,screen,location,frame,history(browser object model)aside from document behavior similarly as window(alert /confirm/prompt is the part of bom)
+//  Dom and Bom
+
+//  Dom navigation--->root element <HTML>,<body>,<head>,-->body->a,p,h1.....etc
+
+//  Searching and getting Elements Refernce...
+
+// document.body.childNodes-->Count the total number of tags present in the childnode.
+// and if text field is shown that means it gives ---->Space,enter
+
+// document.body.children--->gives indiviual section.... div-1 ,div-2,section,etc
+// hasChilNode--->Check the child node presnt or not
+
+// change the element by the help of two way 1.Attribute document.getElementById('heading').innerHTML="hello"
+// way -2 const inn= document.getElementById('head'); inn.inerHtml ="Helo"
+
+//document.getElementByiD -----> fatser for finding the data ofany section from document.queryselector--->queeryelector suposrts tha web after browser 9
